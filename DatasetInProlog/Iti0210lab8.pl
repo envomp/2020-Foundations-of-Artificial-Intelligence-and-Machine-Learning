fd48311de2d4ad20e296f629e9c576c3de659dce
envomp <envomp@ttu.ee>
2020-03-21 18:06:29 +0200

    Teadmusbaas Prologis Tested with Swi-pl

--- DatasetInProlog/Iti0210lab8.pl
+++ DatasetInProlog/Iti0210lab8.pl
@@ -1,19 +1,26 @@
 
 man(marcus).
 man(jaan).
+man(francesco).
 
 pompej_civilian(francesco).
 pompej_civilian(marcus).
 
+birth_date(enrico, 1999).
 birth_date(marcus, 40).
 birth_date(jaan, 1977).
 birth_date(francesco, 1989).
 
 mortal(man).
-eradication(pompej_civilian, 79).
-max_mortality(150).
 
-alive(Year, Human) :- max_mortality(Mortality), birth_date(Human, Birth), Year - Birth =< Mortality, Year >= Birth,
+eradication(pompej_civilian, 79).
+lifetime(mortal, 150).
+
+is_mortal(Name) :- man(Name), mortal(man).
+
+alive(Year, Human) :- not(is_mortal(Human)), birth_date(Human, Birth), Year >= Birth. % is immortal
+
+alive(Year, Human) :- is_mortal(Human), mortal(man), lifetime(mortal, Mortality), birth_date(Human, Birth), Year - Birth =< Mortality, Year >= Birth,
                       (
                             (pompej_civilian(Human), eradication(pompej_civilian, Eradication_year),
                                 (
@@ -57,4 +64,16 @@ alive(Year, Human) :- max_mortality(Mortality), birth_date(Human, Birth), Year -
 %   true
 %
 %   ?- alive(1988, francesco).
+%   false. # not born yet
+%
+%   ?- alive(3019, enrico).
+%   true . # I'm not mortal
+%
+%   ?- alive(2019, enrico).
+%   true .
+%
+%   ?- alive(1999, enrico).
+%   true
+%
+%   ?- alive(1998, enrico).
 %   false. # not born yet
